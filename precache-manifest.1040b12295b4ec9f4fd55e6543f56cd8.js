1866c377770bfd8b63a27b6bd38bc01758de1694
dasha <dasha.saldin@gmail.com>
2020-10-10 16:45:00 +0200

    Updates

---
+++ precache-manifest.1040b12295b4ec9f4fd55e6543f56cd8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d4344b2e9f33ee7f746a22b88037927",
+    "url": "/front-end-libraries/index.html"
+  },
+  {
+    "revision": "22b0b88a7abac57c3e5f",
+    "url": "/front-end-libraries/static/css/main.287a53e0.chunk.css"
+  },
+  {
+    "revision": "e2d1b41bdd6946d17997",
+    "url": "/front-end-libraries/static/js/2.729d2417.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/front-end-libraries/static/js/2.729d2417.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22b0b88a7abac57c3e5f",
+    "url": "/front-end-libraries/static/js/main.2f110c8c.chunk.js"
+  },
+  {
+    "revision": "22e66b0b457b48bada46",
+    "url": "/front-end-libraries/static/js/runtime-main.7eea74f6.js"
+  }
+]);
