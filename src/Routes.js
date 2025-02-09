5f66418eae5dfa976fa6808953e9055388dd629f
dasha <dasha.saldin@gmail.com>
2025-02-09 20:23:18 +0100

    deploy on pages

--- src/Routes.js
+++ src/Routes.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import {Route, Switch} from 'react-router-dom';
-import App from './App';
-import MarkdownPage from './MarkdownPage';
-import DrumMachine from './DrumMachine';
-import JSCalculator from './JSCalculator';
-import TimerClock from './TimerClock';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import App from "./App";
+import MarkdownPage from "./MarkdownPage";
+import DrumMachine from "./DrumMachine";
+import JSCalculator from "./JSCalculator";
+import TimerClock from "./TimerClock";
 
 const Routes = () => (
-    <Switch>
-        <Route exact path='/' component={App}/>
-        <Route exact path="/markdown" component={MarkdownPage} />
-        <Route exact path="/drum-machine" component={DrumMachine} />
-        <Route exact path="/js-calculator" component={JSCalculator}/>
-        <Route exact path="/timer-clock" component={TimerClock}/>
-    </Switch>
-)
+  <Switch>
+    <Route exact path="/" component={App} />
+    <Route exact path="./markdown" component={MarkdownPage} />
+    <Route exact path="./drum-machine" component={DrumMachine} />
+    <Route exact path="./js-calculator" component={JSCalculator} />
+    <Route exact path="./timer-clock" component={TimerClock} />
+  </Switch>
+);
 
 export default Routes;
